c2c51912d9e478be441a2d146953a7ca519d1ea3
ettore-galli <ettore.galli@gmail.com>
2024-08-19 20:25:25 +0200

    Base pin in base

--- led_game/base.py
+++ led_game/base.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from collections.abc import Callable
 from typing import Any, TypeAlias
 
 Incomplete: TypeAlias = Any
@@ -17,3 +18,31 @@ class BaseTime(ABC):
     def ticks_diff(self, ticks1: int, ticks2: int) -> int:
         _ = ticks1, ticks2
         return 0
+
+
+class BasePin(ABC):
+    IRQ_RISING: int = 1
+    OUT: int = 1
+    OPEN_DRAIN: int = 2
+    PULL_UP: int = 1
+    IRQ_FALLING: int = 2
+    IN: int = 0
+
+    def __init__(self, pin_id: int, mode: int) -> None:
+        _ = pin_id, mode
+
+    @abstractmethod
+    def on(self) -> None:
+        pass
+
+    @abstractmethod
+    def off(self) -> None:
+        pass
+
+    @abstractmethod
+    def irq(self, handler: Callable, trigger: int) -> Callable[..., Incomplete]:
+        pass
+
+    @abstractmethod
+    def value(self) -> int:
+        pass
--- led_game/game_engine.py
+++ led_game/game_engine.py
@@ -1,35 +1,4 @@
-from abc import ABC, abstractmethod
-from collections.abc import Callable
-
-from base import BaseTime, Incomplete  # type: ignore[import-not-found]
-
-
-class BasePin(ABC):
-    IRQ_RISING: int = 1
-    OUT: int = 1
-    OPEN_DRAIN: int = 2
-    PULL_UP: int = 1
-    IRQ_FALLING: int = 2
-    IN: int = 0
-
-    def __init__(self, pin_id: int, mode: int) -> None:
-        _ = pin_id, mode
-
-    @abstractmethod
-    def on(self) -> None:
-        pass
-
-    @abstractmethod
-    def off(self) -> None:
-        pass
-
-    @abstractmethod
-    def irq(self, handler: Callable, trigger: int) -> Callable[..., Incomplete]:
-        pass
-
-    @abstractmethod
-    def value(self) -> int:
-        pass
+from base import BasePin, BaseTime  # type: ignore[import-not-found]
 
 
 class HardwareInformation:
