b8346aeeee1d72735f04ebbb17ce622115a188b3
ettore-galli <ettore.galli@gmail.com>
2024-11-08 22:39:30 +0100

    Unit tests working

---
+++ platformio/PIOBlink/test/test_blink/test_main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include <unity.h>
+
+void setUp(void) {
+    // set stuff up here
+}
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+void simple_test(void)
+{
+    TEST_ASSERT_EQUAL(33, 33);
+}
+
+void setup()
+{
+    delay(2000);
+
+    UNITY_BEGIN();
+    RUN_TEST(simple_test);
+    UNITY_END();
+}
+
+void loop()
+{
+    delay(1000);
+}
