f6b29ed2d2257342369adaa543dabdfb1b0493a4
ettore-galli <ettore.galli@gmail.com>
2024-12-30 10:57:21 +0100

    7 note pentathonic

--- platformio/pico-sound-dev/src/main.cpp
+++ platformio/pico-sound-dev/src/main.cpp
@@ -6,19 +6,20 @@ void setup()
   Serial.begin(9600);
   Serial.println("Setup...");
   int microseconds = 1000000;
-  for (unsigned int i = 0; i < NUMBER_OF_NOTES; i++)
+  for (unsigned int i = 0; i < ACTUAL_NUMBER_OF_NOTES; i++)
   {
     pinMode(notes[i].pin, OUTPUT);
-    notes[i].delayTimeus = 2 * int(microseconds / notes[i].freq);
+    notes[i].delayTimeus = 2 * int(microseconds / notes_reference[notes[i].note_number].freq);
     Serial.println(notes[i].delayTimeus);
   }
 }
 
 void loop()
 {
+
   unsigned long current = micros();
 
-  for (unsigned int i = 0; i < NUMBER_OF_NOTES; i++)
+  for (unsigned int i = 0; i < ACTUAL_NUMBER_OF_NOTES; i++)
   {
 
     if (current - notes[i].lastTick > notes[i].delayTimeus)
