233e523d25f6c5be74edf20fe3674b708bae8e3b
ettore-galli <ettore.galli@gmail.com>
2025-01-01 16:59:09 +0100

    Simplify logic

--- platformio/pico-sound-dev/src/main.cpp
+++ platformio/pico-sound-dev/src/main.cpp
@@ -5,7 +5,8 @@
 unsigned int PIN_DOWN = 16;
 unsigned int PIN_UP = 17;
 
-unsigned int SET_KEY_DELAY_MS = 100;
+unsigned long KEY_DEBOUNCE_DELAY_US = 100000;
+unsigned long last_press_time = 0;
 
 typedef unsigned int (*NoteNumberChangeFunction)(const unsigned int note_number);
 
@@ -43,32 +44,10 @@ void initializeNoteDelaysFromFrequency()
   }
 }
 
-
-
-void pitchChange(NoteNumberChangeFunction note_number_change)
+void setNoteNumber(unsigned int i, unsigned int note_number)
 {
-  for (unsigned int i = 0; i < ACTUAL_NUMBER_OF_NOTES; i++)
-  {
-    unsigned int control_pin = control_pins[i].control_pin;
-
-    uint32_t control_pin_state = gpio_get(control_pin);
-
-    if (control_pin_state == LOW)
-    {
-      notes[i].note_number = note_number_change(notes[i].note_number);
-      initializeNoteDelaysFromFrequency();
-    }
-  }
-}
-
-void pitchUp()
-{
-  pitchChange(newNoteNumberUp);
-}
-
-void pitchDown()
-{
-  pitchChange(newNoteNumberDown);
+  notes[i].note_number = note_number;
+  initializeNoteDelaysFromFrequency();
 }
 
 void setup()
@@ -82,6 +61,7 @@ void loop()
 {
 
   unsigned long current = micros();
+
   uint32_t up_pin_state = gpio_get(PIN_UP);
   uint32_t down_pin_state = gpio_get(PIN_DOWN);
 
@@ -95,21 +75,23 @@ void loop()
     uint32_t pitch_up = (control_pin_state == LOW) && (up_pin_state == LOW) && (down_pin_state == HIGH);
     uint32_t pitch_down = (control_pin_state == LOW) && (up_pin_state == HIGH) && (down_pin_state == LOW);
 
+    if (pitch_up && current - last_press_time > KEY_DEBOUNCE_DELAY_US)
+    {
+      setNoteNumber(i, newNoteNumberUp(notes[i].note_number));
+      last_press_time = current;
+    }
+
+    if (pitch_down && current - last_press_time > KEY_DEBOUNCE_DELAY_US)
+    {
+      setNoteNumber(i, newNoteNumberDown(notes[i].note_number));
+      last_press_time = current;
+    }
+
     if ((current - notes[i].lastTick > notes[i].delayTimeus) && play_note)
     {
       notes[i].status = !notes[i].status;
       gpio_put(notes[i].pin, notes[i].status);
       notes[i].lastTick = current;
-    }
-    if (pitch_up)
-    {
-      pitchUp();
-      delay(SET_KEY_DELAY_MS);
-    }
-    if (pitch_down)
-    {
-      pitchDown();
-      delay(SET_KEY_DELAY_MS);
     }
   }
 }
