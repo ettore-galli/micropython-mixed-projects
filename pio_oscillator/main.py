872a33135b5cc89d45f4249654d41bfa81c6bf7c
ettore-galli <ettore.galli@gmail.com>
2026-06-27 21:20:16 +0200

    chase

--- pio_oscillator/main.py
+++ pio_oscillator/main.py
@@ -1,7 +1,8 @@
 import rp2
-from machine import Pin
+from machine import Pin  # pyright: ignore[reportMissingModuleSource]
 import time
-from rp2.asm_pio import *
+
+# from rp2.asm_pio import *  # pyright: ignore[reportMissingModuleSource]
 
 
 @rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
@@ -21,6 +22,30 @@ def blink_1hz():
     jmp(x_dec, "delay_low")
 
 
+@rp2.asm_pio(
+    set_init=(
+        rp2.PIO.OUT_LOW,
+        rp2.PIO.OUT_LOW,
+        rp2.PIO.OUT_LOW,
+        rp2.PIO.OUT_LOW,
+        rp2.PIO.OUT_LOW,
+    )
+)
+def seq5():
+    # sequenza: un LED alla volta
+    set(pins, 0b00001)[31]  # GP16
+    set(pins, 0b00010)[31]  # GP17
+    set(pins, 0b00100)[31]  # GP18
+    set(pins, 0b01000)[31]  # GP19
+    set(pins, 0b10000)[31]  # GP20
+
+
 # Create and start a StateMachine with blink_1hz, outputting on Pin(25)
-sm = rp2.StateMachine(0, blink_1hz, freq=4000, set_base=Pin(15))
-sm.active(1)
+sm0 = rp2.StateMachine(0, blink_1hz, freq=4000, set_base=Pin(15))
+sm0.active(1)
+
+for p in [16, 17, 18, 19, 20]:
+    Pin(p, Pin.OUT)
+
+sm1 = rp2.StateMachine(1, seq5, freq=3000, set_base=Pin(16))
+sm1.active(1)
